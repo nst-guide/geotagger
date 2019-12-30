4cdd79da563c2602f56701ce2d09c9307b8ffa3b
Kyle Barron <barronk@mit.edu>
2019-12-29 21:17:54 -0700

    Add selector to switch map style

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,3 @@
-// @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
@@ -6,13 +5,12 @@ import styles from './Home.css';
 import Map from './Map';
 
 export default class Home extends Component {
-
   render() {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
-        <Map/>
+        <Map />
       </div>
     );
   }
--- app/components/Map.js
+++ app/components/Map.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import fs from 'fs-extra';
+import Select from 'react-select';
+
+require('dotenv').config();
+
+const mapStyleOptions = [
+  {
+    value:
+      'https://raw.githubusercontent.com/nst-guide/osm-liberty-topo/gh-pages/style.json',
+    label: 'OSM Liberty Topo'
+  },
+  { value: 'mapbox://styles/mapbox/outdoors-v11', label: 'Mapbox Outdoors' },
+  {
+    value: 'mapbox://styles/mapbox/satellite-streets-v11',
+    label: 'Mapbox Satellite Streets'
+  }
+];
 
 export default class Map extends React.Component {
   constructor(props) {
@@ -13,7 +29,8 @@ export default class Map extends React.Component {
         latitude: 37.7577,
         longitude: -122.4376,
         zoom: 8
-      }
+      },
+      mapStyle: mapStyleOptions[0]
     };
   }
 
@@ -25,18 +42,32 @@ export default class Map extends React.Component {
     });
   }
 
+  handleChange = mapStyle => {
+    this.setState({ mapStyle }, () =>
+      console.log(`Option selected:`, this.state.mapStyle)
+    );
+  };
+
   render() {
-    const { geojsonData, viewport } = this.state;
+    const { geojsonData, viewport, mapStyle } = this.state;
     return (
-      <ReactMapGL
-        {...viewport}
-        mapStyle="https://raw.githubusercontent.com/nst-guide/osm-liberty-topo/gh-pages/style.json"
-        onViewportChange={viewport => this.setState({ viewport })}
-      >
-        <Source id="my-data" type="geojson" data={geojsonData}>
-          <Layer type="circle" />
-        </Source>
-      </ReactMapGL>
+      <div>
+        <Select
+          value={mapStyle}
+          onChange={this.handleChange}
+          options={mapStyleOptions}
+        />
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
+          mapStyle={mapStyle.value}
+          onViewportChange={viewport => this.setState({ viewport })}
+        >
+          <Source id="my-data" type="geojson" data={geojsonData}>
+            <Layer type="circle" />
+          </Source>
+        </ReactMapGL>
+      </div>
     );
   }
 }
