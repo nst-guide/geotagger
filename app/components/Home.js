22534e2006cb43c32f817021b701a0764a6245fd
Kyle Barron <barronk@mit.edu>
2019-12-29 22:12:56 -0700

    Working example image

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,6 +8,11 @@ export default class Home extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
+        <img
+          src="/Users/kyle/Pictures/Photos Library.photoslibrary/originals/A/A1DFEE5C-483C-4B7A-990F-37B4F7CC0728.jpeg"
+          alt="Error loading"
+          height={400}
+        />
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
         <Map />
