a267b4fd3f95172c3393e67641f057f0d382ba0b
Kyle Barron <barronk@mit.edu>
2019-12-30 01:20:50 -0700

    Working heic image display

--- app/components/MyImage.js
+++ app/components/MyImage.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import execa from 'execa';
+import pathlib from 'path';
 
 export default class Map extends React.Component {
-  convertImage = imagePath => {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      imagePath: null
+    };
+  }
 
-  render() {
+  async componentDidUpdate(prevProps) {
+    if (prevProps === this.props) {
+      return;
+    }
     const { path } = this.props;
     const ext = path.split('.').pop();
     const validExt = ['png', 'jpg', 'jpeg'].includes(ext);
+    const newPath = await this.convertImage(path);
+    this.setState({ imagePath: newPath });
+  }
+
+  convertImage = async imagePath => {
+    const basename = pathlib.parse(imagePath).name;
+    const newPath = `/Users/kyle/.geotagger/${basename}.jpg`;
+    await execa('sips', [imagePath, '-s', 'format', 'jpeg', '--out', newPath]);
+    console.log(newPath);
+    return newPath;
+  };
+
+  render() {
+    const { imagePath } = this.state;
     return (
       <div>
-        {validExt ? (
-          <img src={path} alt="Error loading" height={400} />
-        ) : (
-          <p>Invalid image extension</p>
-        )}
+        {imagePath && <img src={imagePath} alt="Error loading" height={400} />}
       </div>
     );
   }
